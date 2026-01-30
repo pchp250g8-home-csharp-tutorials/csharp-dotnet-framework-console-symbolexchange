2ee659d018caea9385a15f7fb9e67cd67782b227
Alexander <pchp250g8@tutamail.com>
2026-01-30 15:56:12 +0300

    Добавьте файлы проекта.

---
+++ SymbolExchange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolExchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string");
+            var strLine1 = Console.ReadLine();
+            var nLen = strLine1.Length;
+            var strLine2 = strLine1[nLen - 1] + strLine1.Substring(1, nLen - 2) + strLine1[0];
+            Console.WriteLine("Exchange the first and the last symbol in the string");
+            Console.WriteLine("Source string:{0}", strLine1);
+            Console.WriteLine("Destinstion string:{0}", strLine2);
+            Console.Read();
+        }
+    }
+}
